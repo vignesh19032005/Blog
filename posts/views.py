e1accce877d44c7f3cf2e2239a1345b65a4053c6
Vignesh L <68436107+vignesh19032005@users.noreply.github.com>
2024-11-02 20:50:32 +0530

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,10 @@
-# posts/views.py
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Post
 
 def index(request):
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 5)  # Show 5 posts per page
+    paginator = Paginator(post_list, 5)  
     
     page = request.GET.get('page')
     posts = paginator.get_page(page)
@@ -14,7 +13,7 @@ def index(request):
         'posts': posts,
         'page_title': "Vignesh's Blog - Home"
     }
-    return render(request, 'posts/index.html', context)  # Changed from 'blog' to 'posts'
+    return render(request, 'posts/index.html', context)  
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -22,4 +21,4 @@ def post_detail(request, pk):
         'post': post,
         'page_title': f"{post.title} - Vignesh's Blog"
     }
-    return render(request, 'posts/post_detail.html', context)  # Changed from 'blog' to 'posts'
+    return render(request, 'posts/post_detail.html', context)  
